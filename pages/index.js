da350bf213e9f2fdbcff2626270c0684219dec22
Luis Alberto Ruiz Sarmiento <lars5360@gmail.com>
2020-06-25 08:13:33 -0500

    Introducción getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,61 @@
 export default class extends React.Component {
+  static async getInitialProps() {
+    let req = await fetch('https://api.audioboom.com/channels/recommended');
+    let { body: channels } = await req.json();
+    return { channels };
+  }
+
   render() {
+    const { channels } = this.props;
     return (
       <>
-        <h1>Hola mundo!</h1>
-        <p>Holasss</p>
+        <header>Podcast</header>
+
+        <div className="channels">
+          {channels.map(channel => (
+            <div className="channel">
+              <img src={channel.urls.logo_image.original} alt={channel.title}/>
+              <h2>{channel.title}</h2>
+            </div>
+          ))}
+        </div>
 
         <style jsx>{`
-          h1 {
-            color: green;
+          :global(*) {
+            box-sizing: border-box;
           }
-          p {
-            color: red;
+          :global(body) {
+            padding: 0;
+            margin: 0;
+            font-family: system-ui;
+          }
+          header {
+            color: #ffffff;
+            background: #8756ca;
+            padding: 15px;
+            text-align: center;
+          }
+          .channels {
+            display: grid;
+            grid-gap: 15px;
+            padding: 15px;
+            grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+          }
+          .channel {
+            display: block;
+            border-radius: 3px;
+            box-shadow: 0 2px 6px rgba(0,0,0,0.15);
+            margin-bottom:0.5em;
+          }
+          .channel img {
+            width: 100%;
+          }
+          .channel h2 {
+            padding: 5px;
+            font-size: 0.9em;
+            font-weaight: 600;
+            margin: 0;
+            text-align: center;
           }
         `}</style>
       </>
